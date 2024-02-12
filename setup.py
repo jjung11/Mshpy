31a484d65a29d32bbf413115ef6729447f0bca68
Jaewoong Jung <jjung15@gs67-5149629.local>
2024-02-11 20:34:18 -0500

    Included sc_data.py in Msh_sc_data.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+DESCRIPTION = 'Simplified Earth magnetosheath model'
+
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION =  (this_directory / "README.md").read_text()
+
+setup(
+    name='Mshpy',
+    version='0.0.42',
+    packages=find_packages(),
+    package_data={'Mshpy': ['data/**/*']},
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'sympy'
+    ],
+    include_package_data=True,
+)
