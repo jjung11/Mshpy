83390d3f825b91a44cab5f920b2b5740bd1f2b0e
jjung11 <jaewoong.jung@uri.edu>
2025-07-15 18:25:00 -0400

    Output setting

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION =  (this_directory / "README.md").read_text()
 
 setup(
     name='Mshpy',
-    version='0.0.42',
+    version='0.1.1',
     packages=find_packages(),
     package_data={'Mshpy': ['data/**/*']},
     description=DESCRIPTION,
